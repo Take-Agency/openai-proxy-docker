e9c2f2cdfb4736224e4bfca8b1d786895a067e3b
Andriy Katkov <andriy.katkov@gmail.com>
2024-06-20 17:09:52 -0700

    add rate limiter and ip test route

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
-const express = require('express')
-const { createProxyMiddleware } = require('http-proxy-middleware');
-const app = express()
-const port = process.env.PORT || 9017
-const target = process.env.TARGET || 'https://api.openai.com'
+import express from 'express';
+import { createProxyMiddleware } from 'http-proxy-middleware';
+import { rateLimit } from 'express-rate-limit';
+
+const app = express();
+const port = process.env.PORT || 9017;
+const target = process.env.TARGET || 'https://api.openai.com';
 const apiKey = process.env.OPENAI_API_KEY;
+
+const limiter = rateLimit({
+    windowMs: 60 * 1000, // 1 minutes
+    limit: 3, // Limit each IP to 100 requests per `window` (here, per 1 minute).
+    standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+    // store: ... , // Redis, Memcached, etc. See below.
+    keyGenerator: (req, res) => req.headers['do-connecting-ip'] || req.ip,
+});
+
+// Apply the rate limiting middleware to all requests.
+app.use(limiter);
+
+app.get('/ip', (req, res) => res.send({
+    'test': 'yo',
+    'do-connecting-ip': req.headers['do-connecting-ip'],
+    'ip': req.ip,
+}));
 
 app.use('/', createProxyMiddleware({
     target: target,
@@ -22,4 +42,4 @@ app.use('/', createProxyMiddleware({
 
 app.listen(port, () => {
     console.log(`Proxy agent started: http://localhost:${port}`)
-})
+});
